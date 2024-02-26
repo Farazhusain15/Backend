d077df4cdebae16ed9f26a5b0754e3b2823edeb3
faraz husain <farazhusain1503@gmail.com>
2024-02-26 01:54:03 -0800

    changes in util

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@
 
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
  
 dotenv.config({
 path:'./env'
 })
 
 connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 7000,()=>{
+    console.log(`Server is runing on port ${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log(`mongodb connect fails !!!`,err)
+})
 
 
 
