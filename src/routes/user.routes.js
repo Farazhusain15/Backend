500e7eb21561d547cfd6f63eb8e06619350281d9
faraz husain <farazhusain1503@gmail.com>
2024-02-29 02:30:31 -0800

    changes in models

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,20 @@
 import {Router} from "express";
 import { loginUser, registeUser } from "../controllers/user.controller.js";
-
+import {upload} from '../middlewares/multer.middleware.js'
 const router=Router();
 
-router.route('/register').post(registeUser)
+router.route('/register').post(
+    upload.fields([
+  {
+    name:"avatar",
+    maxCount: 1
+  },
+  {
+    name:"coverImage",
+    maxCount: 1
+  }
+    ]),
+    registeUser)
 router.route('/login').post(loginUser)
 
 export default router;
